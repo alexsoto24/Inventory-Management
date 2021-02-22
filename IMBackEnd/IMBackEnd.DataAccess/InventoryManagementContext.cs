39e89779ac1f788beaafb0078d4ebb84cd8971a8
Alex Soto <alex.soto0624@gmail.com>
2021-02-21 23:40:30 -0800

    Added Data Access Classes

---
+++ IMBackEnd/IMBackEnd.DataAccess/InventoryManagementContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace IMBackEnd.DataAccess
+{
+    public partial class InventoryManagementContext : DbContext
+    {
+        public InventoryManagementContext()
+        {
+        }
+
+        public InventoryManagementContext(DbContextOptions<InventoryManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Inventory> Inventories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Store> Stores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Inventory>(entity =>
+            {
+                entity.HasKey(e => new { e.StoreId, e.ProductId })
+                    .HasName("PK__Inventor__F0C23D6D12499432");
+
+                entity.ToTable("Inventory");
+
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Inventories)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Inventory__Produ__6477ECF3");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Inventories)
+                    .HasForeignKey(d => d.StoreId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Inventory__Store__6383C8BA");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(99);
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.ToTable("Store");
+
+                entity.Property(e => e.Addess)
+                    .IsRequired()
+                    .HasMaxLength(99);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(99);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(99);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(99);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
