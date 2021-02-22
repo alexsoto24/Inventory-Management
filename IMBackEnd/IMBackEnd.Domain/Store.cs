fb5df87ccbcdc1c81db2a94888a2a77d21e34a29
Alex Soto <alex.soto0624@gmail.com>
2021-02-22 00:14:19 -0800

    Added Domain Classes

---
+++ IMBackEnd/IMBackEnd.Domain/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMBackEnd.Domain;
+
+namespace IMBackEnd.Domain
+{
+    public class Store
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public IEnumerable<Product> Inventory { get; set;}
+    }
+}
