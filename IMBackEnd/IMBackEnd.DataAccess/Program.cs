9b8c2cc57d39f0661cfc2b0b5301a9c02aa6b5c0
Alex Soto <alex.soto0624@gmail.com>
2021-02-21 21:11:13 -0800

    Inital Commit

---
+++ IMBackEnd/IMBackEnd.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMBackEnd.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
